372a17aa2082243ae2211c88bd0137ec77cc790e
Antonio-Cristian <antoniocristian1808@gmail.com>
2023-08-03 14:35:29 +0300

    New pages

---
+++ BrainSpark/Client/ExamCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace BrainSpark.Client
+{
+	public class ExamCategory
+	{
+		public string ImgUrl { get; set; } = string.Empty;
+		public string ImgAlt { get; set; } = string.Empty;
+		public string TimeLimit { get; set; } = string.Empty;
+		public string Title { get; set; } = string.Empty;
+		public int Questions { get; set; }
+		public string Difficulty { get; set; } = string.Empty;
+	}
+}
