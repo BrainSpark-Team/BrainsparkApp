33323478e8182de29142f430e33353eb849a1236
Antonio-Cristian <antoniocristian1808@gmail.com>
2023-08-07 15:11:38 +0300

    Trying Azure AD B2C login page

--- BrainSpark/Client/Program.cs
+++ BrainSpark/Client/Program.cs
@@ -1,5 +1,6 @@
 using BrainSpark.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,19 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<RemoteAuthenticationService<RemoteAuthenticationState, RemoteUserAccount, OidcProviderOptions>>();
+
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/openid");
+
+});
+
+
 
 await builder.Build().RunAsync();
